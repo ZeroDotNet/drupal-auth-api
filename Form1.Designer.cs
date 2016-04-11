60397338581b88af41853f2bcce4fe8ef405b2d2
Nicolas Pascual <nicolaspascual@hawkant.com>
2016-04-11 14:35:33 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TestingDrupalAuth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.txtDetail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Location = new System.Drawing.Point(77, 10);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(201, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(77, 36);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(201, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Location = new System.Drawing.Point(12, 62);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(266, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lstResult
+            // 
+            this.lstResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.HorizontalScrollbar = true;
+            this.lstResult.Location = new System.Drawing.Point(12, 91);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(266, 160);
+            this.lstResult.TabIndex = 5;
+            this.lstResult.SelectedIndexChanged += new System.EventHandler(this.lstResult_SelectedIndexChanged);
+            // 
+            // txtDetail
+            // 
+            this.txtDetail.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDetail.Location = new System.Drawing.Point(12, 258);
+            this.txtDetail.Multiline = true;
+            this.txtDetail.Name = "txtDetail";
+            this.txtDetail.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDetail.Size = new System.Drawing.Size(266, 245);
+            this.txtDetail.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 515);
+            this.Controls.Add(this.txtDetail);
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.ListBox lstResult;
+        private System.Windows.Forms.TextBox txtDetail;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CookComputing.XmlRpc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TestingDrupalAuth
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtUsername.Text = "npascual";
+            txtPassword.Text = "majestic";
+            btnLogin.Focus();
+        }
+
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            NewMethod();
+        }
+
+        public void NewMethod()
+        {
+            CookieContainer cookieJar = new CookieContainer();
+            UserDto user = new UserDto(txtUsername.Text, txtPassword.Text, "npascual", "nicolaspascual@hawkant.com");
+            string url = "http://www.mtouton.com/api-users/user/login.json";///user/login.json";
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.ContentType = "application/json";
+                request.Method = "POST";
+                request.CookieContainer = cookieJar;
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    string query = JsonConvert.SerializeObject(user);
+
+                    streamWriter.Write(query);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+                var response = (HttpWebResponse)request.GetResponse();
+                Newtonsoft.Json.Linq.JObject userJson;
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    var responseText = streamReader.ReadToEnd();
+                    lstResult.Items.Add(responseText);
+                    userJson = Newtonsoft.Json.Linq.JObject.Parse(responseText);
+                }
+
+                //Check to see if you're logged in
+                url = "http://www.mtouton.com/api-users/system/connect.json";
+                var newRequest = (HttpWebRequest)WebRequest.Create(url);
+                
+                newRequest.ContentType = "application/json";
+                newRequest.Method = "POST";
+                var token = userJson["token"].ToString();
+                var sessid = userJson["sessid"].ToString();
+                var session_name = userJson["session_name"].ToString();
+                var cookie = new Cookie(session_name, sessid, "/", "www.mtouton.com");
+                cookieJar.Add(cookie);
+                newRequest.CookieContainer = cookieJar;
+                newRequest.Headers.Add("X-CSRF-Token", token);
+                var newResponse = (HttpWebResponse)newRequest.GetResponse();
+                using (var newStreamReader = new StreamReader(newResponse.GetResponseStream()))
+                {
+                    var newResponseText = newStreamReader.ReadToEnd();
+                    lstResult.Items.Add(newResponseText);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Can't access server");
+            }
+        }
+
+        private void lstResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtDetail.Text = string.Empty;
+            JObject element = JObject.Parse(lstResult.SelectedItem.ToString());
+            txtDetail.Text = Newtonsoft.Json.JsonConvert.SerializeObject(element, Formatting.Indented);
+        }
+    }
+    public class UserDto
+    {
+        public string name;
+        public string username;
+        public string email;
+        public string pass;
+
+        public UserDto(string username, string pass, string name = "", string email = "")
+        {
+            this.name = name;
+            this.username = username;
+            this.email = email;
+            this.pass = pass;
+
+        }
+    }
+}
